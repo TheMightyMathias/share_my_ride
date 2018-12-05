5b7a0fff3482fad765700c69dacc9c96f2adaac3
Mathias Sellam <mathias.sellam@essec.edu>
2018-12-05 18:49:10 +0100

    trip creation, trip confirmation page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get '/search', to: "trips#search", as: :search
   get 'profiles', to: 'profiles#show'
 
-  get '/confirmation', to: "trips#confirmation", as: :confirmation
+  get 'trips/:id/confirmation', to: "trips#confirmation", as: :confirmation
 end
