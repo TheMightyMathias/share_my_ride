e9ecc47164d3d2138dda43989d18e828fba106b1
Goga Hayrapetyan <hayrapg@gmail.com>
2018-12-05 14:56:23 +0100

    search fixed final

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -2,4 +2,7 @@ class Trip < ApplicationRecord
   belongs_to :user
   belongs_to :airport
   has_many :ride_mates
+
+  geocoded_by :destination
+  after_validation :geocode, if: :will_save_change_to_destination?
 end
