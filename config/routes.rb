4fa2e4274d35a7053dba3d4f5a3c9d543fff7479
Douglas Bay <douglasbays@gmail.com>
2018-12-05 15:37:13 +0100

    added some profile stuff

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   resources :trips
   get '/search', to: "trips#search", as: :search
+  get 'profiles', to: 'profiles#show'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
