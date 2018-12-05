3abdc877e2ff2ed9a71d233c5b77542e1244fd5c
Goga Hayrapetyan <hayrapg@gmail.com>
2018-12-05 11:53:48 +0100

    search fixed

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,5 +1,5 @@
 class TripsController < ApplicationController
-  def search_results
+  def search
     if params[:query]
       sql_query = "destination ILIKE :query"
       @trips = Trip.where(sql_query, query: "%#{params[:query][:destination]}%").order("created_at DESC").where.not(user: current_user)
