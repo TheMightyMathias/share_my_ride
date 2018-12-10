df8825e9333a17726c36d6b03b768f625c6e4ce5
Mathias Sellam <mathias.sellam@essec.edu>
2018-12-10 11:53:14 +0100

    The destination and its coordinates are saved for each ridemate, so we can then display distance and map accordingly

--- app/controllers/ridemates_controller.rb
+++ app/controllers/ridemates_controller.rb
@@ -4,6 +4,10 @@ class RidematesController < ApplicationController
     @ridemate = Ridemate.new
     @ridemate.trip_id = params["trip_id"]
     @ridemate.user = current_user
+    destination_coordinates = Geocoder.search(session[:search]["destination"]).first.coordinates
+    @ridemate.destination = session[:search]["destination"]
+    @ridemate.longitude = destination_coordinates[1]
+    @ridemate.latitude = destination_coordinates[0]
     if @ridemate.save
       redirect_to confirmation_path(@trip.id)
     else
