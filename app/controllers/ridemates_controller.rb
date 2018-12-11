cdef69d81f23f09259febe0adc35983c20ea6fdc
Ryan Propst <ryanpropst@gmail.com>
2018-12-11 12:50:00 +0100

    added logic to trigger email when joining a trip someone has created with trip details

--- app/controllers/ridemates_controller.rb
+++ app/controllers/ridemates_controller.rb
@@ -1,14 +1,23 @@
 class RidematesController < ApplicationController
+  # after_create :send_trip_confirm
+
   def create
     @trip = Trip.find(params[:trip_id])
     @ridemate = Ridemate.new
     @ridemate.trip = @trip
     @ridemate.user = current_user
     if @ridemate.save
+
       redirect_to confirmation_path(@trip.id)
     else
       flash[:alert] = "You are already on this trip buddy."
       redirect_to request.referer
     end
   end
+
+  # private
+  # def send_trip_confirm
+  #   UserMailer.tripconfirm(self).deliver_now
+  # end
+
 end
--- app/models/ridemate.rb
+++ app/models/ridemate.rb
@@ -2,4 +2,10 @@ class Ridemate < ApplicationRecord
   belongs_to :user
   belongs_to :trip
   validates_uniqueness_of :user_id, scope: :trip_id
+  after_create :send_trip_confirm
+
+  private
+  def send_trip_confirm
+    UserMailer.tripconfirm(self).deliver_now
+  end
 end
