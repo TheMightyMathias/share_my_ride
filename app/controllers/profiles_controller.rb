c60a18cdb23d898c7c01b1e001de860c048d2cef
Douglas Bay <douglasbays@gmail.com>
2018-12-11 12:55:05 +0100

    Added delete feature to trips

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,23 +1,36 @@
 class ProfilesController < ApplicationController
   before_action :set_user, only: [:show, :trips]
 
-  def show
-    set_user
-    @trips = current_user.trips
+  def all
+    @trips = Trip.all
   end
+
+  def show
+
+    # @ridemates = Ridemate.all
+
+    set_user
+    # @trips = current_user.trips
+    # @trips = @trips.where(user: current_user)
+    @trips = Trip.all
+    # @trips = @trip.select do |trip|
+    #   trip.trip_users.include?(set_user)
+    # end
+  end
+
 
   def trips
     @trips = user.trips
   end
 
 
-    private
+  private
 
-    def set_user
-      @user = User.find(current_user.id)
-    end
-
-    def user_params
-      params.require(:user).permit(:first_name, :email, :photo, :email)
-    end
+  def set_user
+    @user = User.find(current_user.id)
   end
+
+  def user_params
+    params.require(:user).permit(:first_name, :email, :photo, :email)
+  end
+end
