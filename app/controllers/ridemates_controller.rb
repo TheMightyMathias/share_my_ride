e39523ea27abb02fa585481a843c1a152b8a2470
Goga Hayrapetyan <hayrapg@gmail.com>
2018-12-13 12:40:01 +0100

    Committing some fixes

--- app/controllers/ridemates_controller.rb
+++ app/controllers/ridemates_controller.rb
@@ -16,10 +16,18 @@ class RidematesController < ApplicationController
     @ridemate = Ridemate.new
     @ridemate.trip = @trip
     @ridemate.user = current_user
-    destination_coordinates = Geocoder.search(session[:search]["destination"]).first.coordinates
-    @ridemate.destination = session[:search]["destination"]
-    @ridemate.longitude = destination_coordinates[1]
-    @ridemate.latitude = destination_coordinates[0]
+    if session[:search]["destination"] == "";
+      @no_destination_message = "As you did not provide a destination, here's a great destination to check out in the city"
+      trip_des = Geocoder.search([@trip.latitude, @trip.longitude]).last.data["address"]["state"]
+      @ridemate.destination = Geocoder.search(trip_des).last.data["display_name"]
+      @ridemate.longitude = Geocoder.search(trip_des).last.data["lon"]
+      @ridemate.latitude = Geocoder.search(trip_des).last.data["lat"]
+    else
+      destination_coordinates = Geocoder.search(session[:search]["destination"]).first.coordinates
+      @ridemate.destination = session[:search]["destination"]
+      @ridemate.longitude = destination_coordinates[1]
+      @ridemate.latitude = destination_coordinates[0]
+    end
     if @ridemate.save
 
       redirect_to confirmation_path(@trip.id)
